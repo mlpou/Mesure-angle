99284bd1695c9566a515cef8f6da98eece0b21cb
mathilde-poulin <mathilde.poulin@usherbrooke.ca>
2024-10-01 15:18:28 -0400

    Ajustement de la focntion

--- ipano.py
+++ ipano.py
@@ -242,12 +242,12 @@ ipano = IPANO('/dev/ttyUSB0')
 ipano.set_zero_position()
 
 
-def measures(altPrecision, azPrecision, pauseTime) :
-    ipano.goto((0-altPrecision)*2, 0*(360/azPrecision))
+def measures(altIterations, altPrecision,  azIterations, pauseTime) :
+    ipano.goto((0-altIterations)*2, 0*(360/azIterations))
     time.sleep(7)
-    for i in range(altPrecision) :
-        for j in range(azPrecision) :
+    for i in range(altIterations) :
+        for j in range(azIterations) :
             time.sleep(pauseTime)
-            ipano.goto((i-altPrecision/2)*2, j*(360/azPrecision))
+            ipano.goto((i-altIterations/2)*altPrecision, j*(360/azIterations))
 
-measures(20, 20, 2)
+measures(20, 2, 20, 2)
