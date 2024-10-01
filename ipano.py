bdd75c0567d8ffe1e593de6cc6bcdbd944b9f3d6
mlpou <mathilde.poulin@gmail.com>
2024-10-01 13:43:58 -0400

    Après tests

--- ipano.py
+++ ipano.py
@@ -237,12 +237,7 @@ print(list(serial.tools.list_ports.comports()))
 ipano = IPANO('/dev/ttyUSB0')
 ipano.set_zero_position()
 
-def measures (altPrecision, azPrecision, pauseTime) : 
-    """La procédure contrôle les mouvements pour une prise de mesure d'angles"""
-    for i in range(altPrecision) :
-        for j in range(azPrecision) :
-            time.sleep(pauseTime)
-         ipano.goto(i*360/(altPrecision)-180, j*360/(azPrecision))
-
-
-measures(40, 20, 5)
+for i in range(20) :
+    for j in range(20) :
+        time.sleep(2)
+        ipano.goto(i*5, j*18)
